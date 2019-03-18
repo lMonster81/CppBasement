69e441fdbe82a2db151c1df5c1c38043854ea23f
lMonster81 <38750146+lMonster81@users.noreply.github.com>
2019-03-18 18:32:52 +0800

    shareptr

---
+++ SharePtr.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+template<typename T>
+class SharePtr
+{
+    private:
+      T *ptr;
+      int *count;
+    public:
+    SharePtr(T *newptr):ptr(newptr), count(new int(1))
+    {
+        cout << (*ptr) << "  number  " << (*count) << endl;
+    }
+    SharePtr(SharePtr &sp)
+    {
+        cout << "copy" << endl;
+        ptr = sp.ptr;
+        count = sp.count;
+        (*count)++;
+    }
+    ~SharePtr()
+    {
+        Release();
+    }
+    SharePtr& operator=(const SharePtr &sp)
+    {
+        if(ptr != sp.ptr)
+        {
+            ptr = sp.ptr;
+            count = sp.count;
+            sp.Release();
+        }
+        return *this;
+    }
+    void Release()
+    {
+        if(--(*count) == 0)
+        {
+            cout << (*ptr) << "  remain  " << (*count) << endl;
+            delete ptr;
+            delete count;
+        }
+        else
+            cout << (*ptr) << "  remain  " << (*count) << endl;
+    }
+
+    T& operator *()
+    {
+        return *ptr;
+    }
+};
+
+int main()
+{
+    {
+        SharePtr<int> s1(new int(2));
+        SharePtr<int> s2 = s1;
+        cout << *s1 << endl;
+    }
+    cout << "`````````````````" << endl;
+    {
+        SharePtr<string> s1(new string("123123"));
+        SharePtr<string> s2 = s1;
+        SharePtr<string> s3(s2);
+    }
+    cout << "!!!!!!!!!!!!!!!!" << endl;
+    getchar();
+    return 0;
+}
